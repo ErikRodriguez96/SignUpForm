fd809d489cf867d1f81f8c6b23f1f55801e6b8f6
ErikRodriguez96 <tuf93296@temple.edu>
2022-02-09 01:25:46 -0500

    Added password confirmation/compare to CheckAllFields. Refactored CheckAllFields so multiple error messages can be returned at once. Created toast that displays on successful user input. Requested functionality fully implemented.

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -3,6 +3,7 @@ package edu.temple.signupform
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class FormActivity : AppCompatActivity() {
@@ -20,31 +21,47 @@ class FormActivity : AppCompatActivity() {
             var pwConfirm: String = passwordFieldConfirm.text.toString()
 
             var validFields: Boolean = CheckAllFields(nameField, emailField, passwordField, passwordFieldConfirm)
+
+            if (validFields){
+                var toastString = "Hello ".plus(nameField.text).plus(" to the SignUpForm App")
+                val validInputToast = Toast.makeText(applicationContext, toastString, Toast.LENGTH_LONG)
+                validInputToast.show()
+            }
         }
 
     }
 
     private fun CheckAllFields(nameField: EditText, emailField: EditText, passwordField: EditText, passwordFieldConfirm: EditText): Boolean {
+
+        var validFields: Boolean = true
+
         if (nameField.length() === 0) {
             nameField.setError("Name is required")
-            return false
+            validFields = false
         }
 
         if (emailField.length() === 0) {
             emailField.setError("Email is required")
-            return false
+            validFields = false
         }
         if (passwordField.length() === 0) {
             passwordField.setError("Password is required")
-            return false
+            validFields = false
         }
         else if (passwordFieldConfirm.length() === 0) {
             passwordFieldConfirm.setError("Password must be confirmed")
-            return false
+            validFields = false
         }
 
-        // after all validation return true.
-        return true
+        var pw1: String = passwordField.text.toString()
+        var pw2: String = passwordFieldConfirm.text.toString()
+        if (pw1.equals(pw2) == false){
+            passwordFieldConfirm.setError("Passwords must match")
+            validFields = false
+        }
+
+        // after all validation return bool.
+        return validFields
     }
 
 
